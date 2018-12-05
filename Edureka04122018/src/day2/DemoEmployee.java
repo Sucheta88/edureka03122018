e124234479dbd7bb3e8f0fe2cf08953bf0b55361
Munish Kumar <goformunish@gmail.com>
2018-12-05 13:56:59 +0100

    Third Commit

--- Edureka04122018/src/day2/DemoEmployee.java
+++ Edureka04122018/src/day2/DemoEmployee.java
@@ -4,10 +4,10 @@ public class DemoEmployee {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Employee sucheta = new Employee();
-		sucheta.salary = 9000;
-		sucheta.bonus = 10000;
-		sucheta.calculateSalary();
+		Employee su = new Employee();
+		su.salary = 9000;
+		su.bonus = 10000;
+		su.calculateSalary();
 
 	}
 
